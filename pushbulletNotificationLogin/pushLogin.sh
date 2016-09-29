e41cd7b0f8d420b2fe462a38d526ca26206d3505
Toni Miquel <netyaco@gmail.com>
2016-09-29 19:41:18 +0200

    Create pushLogin.sh

---
+++ pushbulletNotificationLogin/pushLogin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="YOURKEY"
+
+#Using PAM
+MSG="Login from user $PAM_USER"
+
+#Using profile
+#MSG="Login from user $USER"
+
+#Only execute on login, not on logout
+if [ "$PAM_TYPE" != "close_session" ]; then
+
+curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="Raspi Alert" -d body="$MSG"
+fi
+
+exit 0
