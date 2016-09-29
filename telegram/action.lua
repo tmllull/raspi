c70181ca09242dddf8008ef0c930b7c3f1d7695b
Toni Miquel <netyaco@gmail.com>
2016-09-29 19:51:45 +0200

    Update action.lua

--- telegram/action.lua
+++ telegram/action.lua
@@ -5,6 +5,13 @@ function on_msg_receive (msg)
   if (msg.text=='ping') then
      send_msg (msg.from.print_name, 'pong', ok_cb, false)
   end
+  if (msg.text=='temp') then
+         local line = io.popen("cat /sys/devices/virtual/thermal/thermal_zone0/temp")
+         local result = line:read("*a")
+         line:close()
+         result = result/1000
+         send_msg (msg.from.print_name, 'My temperature is: '..result..' ºC', ok_cb, false)
+  end
 end
 
 function on_our_id (id)
