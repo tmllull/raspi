d86b6a2247dc05af6e08ad15ff41cbf432d3ad75
Toni Miquel <netyaco@gmail.com>
2016-10-22 14:35:29 +0200

    Delete pushLogout.sh

--- mailNotificationLogin/pushLogout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-API="YOURKEY"
-MSG="Logout from user $PAM_USER"
-#MSG="Logout from user $USER"
-
-curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="Raspi Alert" -d body="$MSG"
-
-exit 0
